5adf8298ca04c7296ff5ec4e8f664c7465bcd748
tan-yasu <tandangassassin@gmail.com>
2024-06-14 21:01:09 +0700

    kslknsklfn

---
+++ Shopping_Tutorial/Controllers/CheckoutController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping_Tutorial.Repository;
+using System.Security.Claims;
+
+namespace Shopping_Tutorial.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public CheckoutController(DataContext context)
+        {
+            _dataContext = context;
+        }
+        public async Task<IActionResult> Checkout()
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            if (userEmail == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                var ordercode = Guid.NewGuid().ToString();
+                var orderItem = new OrderModel();
+                orderItem.Ordercode = ordercode;
+                orderItem.Username = userEmail;
+                orderItem.Startus = 1;
+                orderItem.CreatedDate = DateTime.Now;
+                _dataContext.Add(orderItem);
+                _dataContext.SaveChanges();
+                TempData["success"] = "Đơn Hàng Đã Được Tạo";
+                return RedirectToAction("index","cart");
+
+            }
+                return View();
+            
+            }
+        }
+    }
