6e4c5df7eb4801f4cce8244944b60f0c19044bea
ledinhnhat1102 <nhat6870k@gmail.com>
2024-06-15 08:10:16 +0700

    33 ne

--- Shopping_Tutorial/Program.cs
+++ Shopping_Tutorial/Program.cs
@@ -1,12 +1,14 @@
 using Shopping_Tutorial.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Shopping_Tutorial.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Connection db
 builder.Services.AddDbContext<DataContext>(options =>
 {
-	options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectedDb"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectedDb"));
 });
 
 // Add services to the container.
@@ -15,8 +17,24 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(option =>
 {
-	option.IdleTimeout = TimeSpan.FromMinutes(30);
-	option.Cookie.IsEssential = true;
+    option.IdleTimeout = TimeSpan.FromMinutes(30);
+    option.Cookie.IsEssential = true;
+});
+
+builder.Services.AddIdentity<AppUserModel, IdentityRole>()
+    .AddEntityFrameworkStores<DataContext>()  // S?a l?i t?i ?ây
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 4;
+
+    options.User.RequireUniqueEmail = true;
 });
 
 var app = builder.Build();
@@ -28,21 +46,23 @@ app.UseSession();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllerRoute(
     name: "Areas",
     pattern: "{area:exists}/{controller=Product}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
-	name: "category",
-	pattern: "/category/{Slug?}",
-	defaults: new { controller = "Category", action = "Index" });
+    name: "category",
+    pattern: "/category/{Slug?}",
+    defaults: new { controller = "Category", action = "Index" });
 
 app.MapControllerRoute(
     name: "brand",
@@ -50,8 +70,8 @@ app.MapControllerRoute(
     defaults: new { controller = "Brand", action = "Index" });
 
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 //Seeding Data
