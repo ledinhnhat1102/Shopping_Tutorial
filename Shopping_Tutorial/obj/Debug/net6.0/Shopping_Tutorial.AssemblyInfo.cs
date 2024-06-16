81ee1139baf061cd7c25b436724a7b92ebff6b10
ledinhnhat1102 <nhat6870k@gmail.com>
2024-06-16 16:00:41 +0700

    bai 41

--- Shopping_Tutorial/obj/Debug/net6.0/Shopping_Tutorial.AssemblyInfo.cs
+++ Shopping_Tutorial/obj/Debug/net6.0/Shopping_Tutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopping_Tutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe4508123b181d07023226e2baf253ab90ab5d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee611ae539a0d3d00f6b56abbb0ab2d5d21291f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopping_Tutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopping_Tutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
